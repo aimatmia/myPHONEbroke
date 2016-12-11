64cacebf9189372a1f3f066165b972b141104732
Mia <mobrien@stuy.edu>
2016-12-11 17:03:35 -0500

    Update rewrite.c

--- rewrite.c
+++ rewrite.c
@@ -18,30 +18,25 @@ int main(){
   sb.sem_flg = SEM_UNDO;
   sb.sem_op = -1;
 
-
   semid = semget( key, 1, IPC_EXCL | O_APPEND | 0644);
-
   semop(semid, &sb, 1);
-
-	fd = open("ring", O_APPEND | O_RDWR | 0644);
+  fd = open("ring", O_APPEND | O_RDWR | 0644);
   size = (int *) shmat( shmget( key, sizeof(int), 0644 ) );   //size of last line 
 
   lseek(fd, -(*size), SEEK_END);
   char line[(*size) + 1];   //previous line
-  
-int rd = read(fd, line, *size); //reading descriptor
-line[rd] = '\0'
-printf("Last Message: %s\n", line);
-               char next[100];
-	printf("Enter a message: ");
-	fgets(next, sizeof(next), stdin);
-	*size = strlen(next);
-
-	write(fd, next, *size);
-	close(fd);
-
-	sb.sem_op = 1;
-    semop(semid, &sb, 1);
+  int rd = read(fd, line, *size); //reading descriptor
+  line[rd] = '\0'
+  printf("Last Message: %s\n", line);
+	
+  char next[100];
+  printf("Enter a message: ");
+  fgets(next, sizeof(next), stdin);
+  *size = strlen(next);
+  write(fd, next, *size);
+  close(fd);
+  sb.sem_op = 1;
+  semop(semid, &sb, 1);
 
   return 0;
 }
